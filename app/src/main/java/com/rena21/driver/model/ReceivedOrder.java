c207fdb2d960d67a7d2219e091c167a20bbba5bb
jaehong <newbie302@gmail.com>
2017-04-30 21:50:04 +0900

    received orders recyclerview의 item에 timestamp 추가

--- app/src/main/java/com/rena21/driver/model/ReceivedOrder.java
+++ app/src/main/java/com/rena21/driver/model/ReceivedOrder.java
@@ -2,10 +2,12 @@ package com.rena21.driver.model;
 
 public class ReceivedOrder {
 
+    public String timeStamp;
     public String restaurantName;
     public String orderItems;
 
-    public ReceivedOrder(String restaurantName, String orderItems) {
+    public ReceivedOrder(String timeStamp, String restaurantName, String orderItems) {
+        this.timeStamp = timeStamp;
         this.restaurantName = restaurantName;
         this.orderItems = orderItems;
     }
