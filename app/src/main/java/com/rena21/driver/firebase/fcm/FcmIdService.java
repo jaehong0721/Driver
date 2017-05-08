8cd43dc1fc31189875196c6aa4ef1873d92f8433
myeesan <myeesan@gmail.com>
2017-05-08 19:53:04 +0900

    Add fcm services

---
+++ app/src/main/java/com/rena21/driver/firebase/fcm/FcmIdService.java
@@ -0,0 +1,32 @@
+package com.rena21.driver.firebase.fcm;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.rena21.driver.etc.AppPreferenceManager;
+
+/**
+ * 출처: https://firebase.google.com/docs/cloud-messaging/android/client?hl=ko
+ */
+public class FcmIdService extends FirebaseInstanceIdService {
+
+    private AppPreferenceManager appPreferenceManager;
+
+    @Override public void onCreate() {
+        super.onCreate();
+        appPreferenceManager = new AppPreferenceManager(this);
+    }
+
+    @Override
+    public void onTokenRefresh() {
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        storeToken(refreshedToken);
+    }
+
+    public void storeToken(String token) {
+        appPreferenceManager.setFcmToken(token);
+        Log.d("firebase", "Refreshed token: " + token);
+    }
+
+}
