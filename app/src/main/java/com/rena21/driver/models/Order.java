9a24d4c8709929fd614261d4cd9bf7b30ac43602
jaehong <newbie302@gmail.com>
2017-05-24 00:31:02 +0900

    Order model에 delivered, isDelivered 추가

--- app/src/main/java/com/rena21/driver/models/Order.java
+++ app/src/main/java/com/rena21/driver/models/Order.java
@@ -5,6 +5,8 @@ import java.util.List;
 public class Order{
     public List<OrderItem> orderItems;
     public boolean accepted;
+    public boolean delivered;
+    public long deliveredTime;
 
     public Order() {}
 }
