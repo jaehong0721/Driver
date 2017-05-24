447e7756b805f45e84133dfc4d43460f93b30fc9
jaehong <newbie302@gmail.com>
2017-05-24 19:26:42 +0900

    delivery detail activity 진입 시 식당 이름 표시

--- app/src/main/java/com/rena21/driver/activities/DeliveryDetailActivity.java
+++ app/src/main/java/com/rena21/driver/activities/DeliveryDetailActivity.java
@@ -4,7 +4,12 @@ import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.rena21.driver.R;
+import com.rena21.driver.firebase.FirebaseDbManager;
 import com.rena21.driver.view.actionbar.ActionBarViewModel;
 import com.rena21.driver.view.layout.DeliveryOrderTabsLayout;
 
@@ -14,6 +19,11 @@ public class DeliveryDetailActivity extends BaseActivity implements DeliveryOrde
     private TextView tvRestaurantName;
     private DeliveryOrderTabsLayout tabsLayout;
 
+    private FirebaseDbManager dbManager;
+
+    private String fileName;
+    private String vendorPhoneNumber;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,9 +32,27 @@ public class DeliveryDetailActivity extends BaseActivity implements DeliveryOrde
         ActionBarViewModel.createWithActionBar(getSupportActionBar())
                 .setTitle("납품상세");
 
-        tvRestaurantName = (TextView) findViewById(R.id.tvRestaurantName);
-        tabsLayout = (DeliveryOrderTabsLayout) findViewById(R.id.deliveryDetailTab);
+        fileName = getIntent().getStringExtra("fileName");
+        vendorPhoneNumber = getIntent().getStringExtra("vendorPhoneNumber");
 
+        dbManager = new FirebaseDbManager(FirebaseDatabase.getInstance(), vendorPhoneNumber);
+
+        tvRestaurantName = (TextView) findViewById(R.id.tvRestaurantName);
+        final String restaurantPhoneNumber = getPhoneNumber(fileName);
+        dbManager.getRestaurantName(restaurantPhoneNumber, new ValueEventListener() {
+            @Override public void onDataChange(DataSnapshot dataSnapshot) {
+                String restaurantName = dataSnapshot.getValue(String.class);
+                if (restaurantName == null) {
+                    tvRestaurantName.setText(restaurantPhoneNumber);
+                } else {
+                    tvRestaurantName.setText(restaurantName);
+                }
+            }
+
+            @Override public void onCancelled(DatabaseError databaseError) { }
+        });
+
+        tabsLayout = (DeliveryOrderTabsLayout) findViewById(R.id.deliveryDetailTab);
         tabsLayout.setTabClickListener(this);
         tabsLayout.showInitialView();
     }
@@ -43,5 +71,9 @@ public class DeliveryDetailActivity extends BaseActivity implements DeliveryOrde
                 Toast.makeText(this, "bills", Toast.LENGTH_SHORT).show();
                 break;
         }
+    }
+
+    private String getPhoneNumber(String fileName) {
+        return fileName.split("_")[0];
     }
 }
