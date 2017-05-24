83a00e75b6c1028c9af385f81e150e815d659b68
jaehong <newbie302@gmail.com>
2017-05-24 16:11:37 +0900

    delivery detail activity 추가 ...
    장부에서 아이템 목록 클릭 시 연결되는 activity

---
+++ app/src/main/java/com/rena21/driver/activities/DeliveryDetailActivity.java
@@ -0,0 +1,15 @@
+package com.rena21.driver.activities;
+
+import android.os.Bundle;
+
+import com.rena21.driver.R;
+
+
+public class DeliveryDetailActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delivery_detail);
+    }
+}
