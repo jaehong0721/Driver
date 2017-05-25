6329bfc486a15ddfa4cd0bd2c2b32e968de76d53
jaehong <newbie302@gmail.com>
2017-05-25 20:10:39 +0900

    Firebase model class에 @Keep 어노테이션 추가

--- app/src/main/java/com/rena21/driver/models/Order.java
+++ app/src/main/java/com/rena21/driver/models/Order.java
@@ -1,7 +1,10 @@
 package com.rena21.driver.models;
 
+import android.support.annotation.Keep;
+
 import java.util.List;
 
+@Keep
 public class Order{
     public List<OrderItem> orderItems;
     public boolean accepted;
--- app/src/main/java/com/rena21/driver/models/OrderItem.java
+++ app/src/main/java/com/rena21/driver/models/OrderItem.java
@@ -1,18 +1,14 @@
 package com.rena21.driver.models;
 
-import android.os.Parcel;
+import android.support.annotation.Keep;
 
+@Keep
 public class OrderItem {
     public String name;
     public String count;
     public int price;
 
     public OrderItem() {}
-
-    protected OrderItem(Parcel in) {
-        name = in.readString();
-        count = in.readString();
-    }
 
     @Override public String toString() {
         return "OrderItem{" +
