aa684a1c119d540aa1f4d69c48b2d89f65279bce
jaehong <newbie302@gmail.com>
2017-08-08 23:33:32 +0900

    품목 추가 버튼 view 추가

---
+++ app/src/main/java/com/rena21/driver/view/widget/AddMajorItemButton.java
@@ -0,0 +1,30 @@
+package com.rena21.driver.view.widget;
+
+
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+import com.google.android.flexbox.FlexboxLayout;
+import com.rena21.driver.R;
+
+public class AddMajorItemButton extends android.support.v7.widget.AppCompatButton {
+
+    public AddMajorItemButton(Context context) {
+        this(context, null);
+    }
+
+    public AddMajorItemButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        FlexboxLayout.LayoutParams lp = new FlexboxLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        lp.setFlexGrow(1.0f);
+        setLayoutParams(lp);
+
+        setBackground(ContextCompat.getDrawable(context, R.drawable.add_major_item_button_shape));
+        setTextColor(ContextCompat.getColor(context, R.color.white));
+        setTextSize(14);
+        setText("품목추가");
+    }
+}
