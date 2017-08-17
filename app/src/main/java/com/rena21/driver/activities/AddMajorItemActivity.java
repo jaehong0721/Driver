da04925e92abcef288d46657a24ec31dfdbf32db
jaehong <newbie302@gmail.com>
2017-08-18 08:47:42 +0900

    주요품목 추가 액티비티 추가

---
+++ app/src/main/java/com/rena21/driver/activities/AddMajorItemActivity.java
@@ -0,0 +1,16 @@
+package com.rena21.driver.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.rena21.driver.R;
+
+
+public class AddMajorItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_major_item);
+    }
+}
