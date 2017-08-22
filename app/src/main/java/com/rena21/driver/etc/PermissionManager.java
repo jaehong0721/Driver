c937d8443c58e525366b45c55de6711d0613962b
jaehong <newbie302@gmail.com>
2017-08-22 18:56:56 +0900

    외부 저장 장치 접근 권한 추가

--- app/src/main/java/com/rena21/driver/etc/PermissionManager.java
+++ app/src/main/java/com/rena21/driver/etc/PermissionManager.java
@@ -14,8 +14,8 @@ import android.support.v7.app.AlertDialog;
 public class PermissionManager {
 
     public static PermissionManager newInstance(Activity activity) {
-        return new PermissionManager(activity, new String[]{Manifest.permission.READ_PHONE_STATE},
-                "회원가입을 위한 전화번호 권한을 요청합니다.",
+        return new PermissionManager(activity, new String[]{Manifest.permission.READ_PHONE_STATE, Manifest.permission.READ_EXTERNAL_STORAGE},
+                "회원가입을 위한 전화번호 권한과\n업체정보 사진 설정을 위한 접근 권한을 요청합니다.",
                 "앱에서 필요한 권한을 요청을 할 수 없습니다.\n\n" + "서비스를 계속 사용하기 위해서 \"설정\" 버튼을 누르신 후, 권한 탭에서 직접 권한을 허락해 주세요.");
     }
 
