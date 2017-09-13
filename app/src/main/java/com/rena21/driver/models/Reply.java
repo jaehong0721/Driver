798689b4b0a6efa9ba9ce27351930ddf87f51487
jaehong <newbie302@gmail.com>
2017-09-14 03:24:45 +0900

    사용하지 않는 기능 제거

--- app/src/main/java/com/rena21/driver/models/Reply.java
+++ app/src/main/java/com/rena21/driver/models/Reply.java
@@ -2,7 +2,6 @@ package com.rena21.driver.models;
 
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class Reply {
     public ArrayList<RepliedEstimateItem> repliedItems;
@@ -19,17 +18,5 @@ public class Reply {
                 ", totalPrice=" + totalPrice +
                 ", isPicked=" + isPicked +
                 '}';
-    }
-
-    public ArrayList<HashMap<String, String>> getRepliedItemNameMapList() {
-        ArrayList<HashMap<String, String>> itemNameMapList = new ArrayList<>();
-        for(int i = 0; i<repliedItems.size(); i++) {
-            if(repliedItems.get(i).price != 0) {
-                HashMap<String, String> itemNameMap = new HashMap<>();
-                itemNameMap.put("name", repliedItems.get(i).itemName);
-                itemNameMapList.add(itemNameMap);
-            }
-        }
-        return itemNameMapList;
     }
 }
