da3a216435b7c0f47d980290508e2b86c2f0dbde
jaehong <newbie302@gmail.com>
2017-09-13 19:08:32 +0900

    견적 가격 액티비티 레이아웃 구성

--- app/src/main/java/com/rena21/driver/activities/InputPriceOfEstimateActivity.java
+++ app/src/main/java/com/rena21/driver/activities/InputPriceOfEstimateActivity.java
@@ -1,8 +1,10 @@
 package com.rena21.driver.activities;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.rena21.driver.R;
+import com.rena21.driver.view.actionbar.ActionBarWithButtonViewModel;
 
 
 public class InputPriceOfEstimateActivity extends BaseActivity {
@@ -11,5 +13,13 @@ public class InputPriceOfEstimateActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_price_of_estimate);
+
+        ActionBarWithButtonViewModel.createWithActionBar(getSupportActionBar())
+                .setTitle("견적가 입력")
+                .setCloseButtonListener(new View.OnClickListener() {
+                    @Override public void onClick(View v) {
+                        finish();
+                    }
+                });
     }
 }
