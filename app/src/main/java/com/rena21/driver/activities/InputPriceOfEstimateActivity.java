01695261888b764aafbbf31f06cf4aaceaea405b
jaehong <newbie302@gmail.com>
2017-09-13 17:25:48 +0900

    견적 요청 온 아이템들에 대해 가격을 입력할 수 있는 activity 추가

---
+++ app/src/main/java/com/rena21/driver/activities/InputPriceOfEstimateActivity.java
@@ -0,0 +1,15 @@
+package com.rena21.driver.activities;
+
+import android.os.Bundle;
+
+import com.rena21.driver.R;
+
+
+public class InputPriceOfEstimateActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_price_of_estimate);
+    }
+}
